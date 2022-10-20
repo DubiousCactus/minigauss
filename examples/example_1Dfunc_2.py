Fix gradient computation and improve many things

examples/example_1Dfunc_2.py
@@ -23,8 +23,8 @@ def test_function_1D(x):
     return (x * 6 - 2) ** 2 * np.sin(x * 12 - 4)
 
 
-NUM_TRAIN_PTS = 40
-NOISE_STD = 0.9
+NUM_TRAIN_PTS = 60
+NOISE_STD = 1.3
 X_RANGE = (0, 1)
 NUM_TARGET_PTS = 400
 
@@ -41,9 +41,9 @@ y_train += NOISE_STD * rng.standard_normal((NUM_TRAIN_PTS, 1))
 # Maybe in the future we could do: GaussianProcess(x, y, [PolynomialPrior(deg=2), PolynomialPrior(deg=3), SawToothPrior()])
 # and it would optimise for the best prior model as well as hyperparameters.
 gp = GaussianProcess(
-    ConstantFunc(Bound(0, 10)), ExponentialKernel(sigma_y_bounds=Bound(0, 20))
+    ConstantFunc(), ExponentialKernel()
 )
-gp.fit(x_train, y_train, n_restarts=10, lr=2e-5)
+gp.fit(x_train, y_train, n_restarts=10, lr=5e-5, decay_rate=0.9)
 
 # GP model predictions
 f_sample1, mean, mean_var = gp.predict(x_oracle)

examples/example_process_fitting.py
@@ -20,8 +20,8 @@ from minigauss import GaussianProcess
 from minigauss.priors import Bound, ExponentialKernel
 from minigauss.priors.mean import ConstantFunc
 
-NUM_TRAIN_REALIZATIONS = 5
-NUM_TRAIN_PTS_PER_REALIZATION = 30
+NUM_TRAIN_REALIZATIONS = 6
+NUM_TRAIN_PTS_PER_REALIZATION = 60
 MAX_NUM_OBS_PTS = 40
 X_RANGE = (0, 5)
 NUM_TARGET_PTS = 400
@@ -30,7 +30,7 @@ NUM_TARGET_PTS = 400
 rng = default_rng()
 # Oracle process (a preset GP)
 orcale_gp = GaussianProcess(
-    ConstantFunc(value=0), ExponentialKernel(sigma_y=2.3, l=0.4, sigma_n=0.1)
+    ConstantFunc(value=0), ExponentialKernel(sigma_y=2.3, l=0.4, sigma_n=0.8)
 )  # Add a little bit of noise
 
 # Training data: in practice, you may have arrays of measurements from independent realizations of
@@ -46,15 +46,18 @@ for i in range(NUM_TRAIN_REALIZATIONS):
     y_train[i, :] = np.expand_dims(y, axis=1)  # TODO: Return same dim as input!
 
 # Defining our GP that we want to fit to the data (to hopefully learn the oracle hyperparameters)
-gp = GaussianProcess(ConstantFunc(bounds=Bound(-1, 1)), ExponentialKernel())
+# Should fit: ConstantFunc(value=7), ExponentialKernel(sigma_y=2.3, l=0.4, sigma_n=0.1)
+gp = GaussianProcess(ConstantFunc(), ExponentialKernel(), use_scipy=False)
 # Training the gp: to keep things simple in the library, let's merge all data points into one
 # training set.
+# TODO: Implement mini-batch training
 gp.fit(
     np.vstack(x_train),
     np.vstack(y_train),
-    n_restarts=20,
-    max_fast_iterations=100,
-    lr=1e-3,
+    n_restarts=10,
+    max_fast_iterations=50,
+    lr=1e-4,
+    decay_rate=0.99
 )
 
 

examples/plot_custom_GP.py
@@ -23,7 +23,7 @@ X_RANGE = (-1, 1)
 
 rng = default_rng()
 # Prior
-gp = GaussianProcess(ConstantFunc(value=0), ExponentialKernel(sigma_y=1e-5, l=0.4))
+gp = GaussianProcess(ConstantFunc(value=0), ExponentialKernel(sigma_y=1e-5, l=0.4, sigma_n=0))
 x = np.sort((rng.uniform(X_RANGE[0], X_RANGE[1], (100, 1))), axis=0)
 
 y1, mean, mean_var = gp.sample(x)

minigauss/gp.py
@@ -9,12 +9,14 @@
 Simple Gaussian Process Regression with Numpy.
 """
 
+import scipy
 import tqdm
 import pickle
 import signal
 import numpy as np
 
-from typing import Tuple
+from scipy import linalg
+from typing import Optional, Tuple
 from numpy.linalg import LinAlgError
 
 from .priors import MeanPrior, CovariancePrior
@@ -36,7 +38,7 @@ class GaussianProcess:
         self,
         mean_prior: MeanPrior,
         covariance_prior: CovariancePrior,
-        use_scipy=False,
+        use_scipy=True,
     ):
         self._use_scipy = use_scipy
         self._x, self._y = None, None
@@ -77,27 +79,31 @@ class GaussianProcess:
                 self._x, self._x, observations=True  # Add the noise variance estimate
             ),
         )
-        inv_K = np.linalg.inv(K)
+        residuals = self._y - mu
+        if not self._use_scipy:
+            inv_K = np.linalg.inv(K)
+        else:
+            # This is actually slower!
+            solved = linalg.solve(K, residuals, assume_a="pos")
         _, logdet = np.linalg.slogdet(
             K
         )  # Much more robust to overflow than np.linalg.det for larger matrices!
+        data_term = residuals.T @ (inv_K @ residuals if not self._use_scipy else solved)
 
         # Compute the log likelihood
-        return (
-            -0.5 * logdet
-            - 0.5 * ((self._y - mu).T @ inv_K @ (self._y - mu)).item()
-            - n / 2 * np.log(np.pi * 2)
-        )
+        return (-0.5 * logdet) - (0.5 * data_term.item()) - ((n / 2) * np.log(np.pi * 2))
 
     def _optimize_model(
         self,
         eps: float,
         max_iterations: int,
         lr: float,
+        decay_rate: Optional[float] = None,
+        decay_every=50,
         random_init=True,
         init_iter=0,
         init_logml=float("-inf"),
-    ) -> Tuple[float, float]:
+    ) -> Tuple[float, float, float]:
         assert self._x is not None and self._y is not None, "No training data!"
         i, logml, last_logml = init_iter, float("-inf"), init_logml
         if random_init:
@@ -111,25 +117,24 @@ class GaussianProcess:
             while i < max_iterations and self._fitting:
                 pbar.set_description(f"Iteration {i}")
                 pbar.update()
-                try:
-                    # First the mean!
-                    # Set observations=True to add the noise variance estimate
-                    K_inv = np.linalg.inv(
-                        self._covariance_prior(self._x, self._x, observations=True)
-                    )
-                    self._mean_prior.optimize_one_step(
-                        {"K_inv": K_inv}, self._x, self._y, lr
-                    )
-                    # Then the covariance!
-                    mu = self.mean(self._x)
-                    self._covariance_prior.optimize_one_step(
-                        {"mu": mu}, self._x, self._y, lr
-                    )
-                    # Now compute the total log ML
-                    logml = self._log_marginal_likelihood()
-                except LinAlgError as e:
-                    print(e)
-                    break
+                if decay_rate is not None and i % decay_every == 0:
+                    lr = lr * decay_rate ** (i / decay_every)
+                # TODO: Compute updates for mean + kernel, THEN apply updates!
+                # First the mean!
+                # Set observations=True to add the noise variance estimate
+                K_inv = np.linalg.inv(
+                    self._covariance_prior(self._x, self._x, observations=True)
+                )
+                mu = self.mean(self._x)
+                # Then the covariance!
+                self._mean_prior.optimize_one_step(
+                    {"K_inv": K_inv}, self._x, self._y, lr
+                )
+                self._covariance_prior.optimize_one_step(
+                    {"mu": mu}, self._x, self._y, lr
+                )
+                # Now compute the total log ML
+                logml = self._log_marginal_likelihood()
                 pbar.set_postfix_str(f"Log marginal likelihood: {logml:.2f}")
                 if logml in [float("-inf"), float("+inf")]:
                     return i, float("-inf")
@@ -137,16 +142,17 @@ class GaussianProcess:
                     break
                 last_logml = logml
                 i += 1
-        return i, logml
+        return i, logml, lr
 
     def fit(
         self,
         x: np.ndarray,
         y: np.ndarray,
         n_restarts=10,
-        lr=1e-3,
+        lr=3e-5,
+        decay_rate: Optional[float] = 0.9,
         eps=1e-5,
-        max_fast_iterations=1000,
+        max_fast_iterations=100,
         max_final_iterations=10000,
     ):
         """
@@ -167,11 +173,14 @@ class GaussianProcess:
 
         for n in range(n_restarts):
             print(f"[*] Optimizing model {n+1}/{n_restarts}...")
-            iteration, logml = self._optimize_model(1e-2, max_fast_iterations, lr)
+            iteration, logml, last_lr = self._optimize_model(
+                1e-2, max_fast_iterations, lr, decay_every=50, decay_rate=decay_rate
+            )
             models[logml] = {
                 "mean": pickle.dumps(self._mean_prior),
                 "cov": pickle.dumps(self._covariance_prior),
                 "iteration": iteration,
+                "lr": last_lr,
             }
         best_logml = max(models.keys())
         best_model = models[best_logml]
@@ -180,13 +189,15 @@ class GaussianProcess:
         )
         self._mean_prior = pickle.loads(best_model["mean"])
         self._covariance_prior = pickle.loads(best_model["cov"])
-        _, logml = self._optimize_model(
+        _, logml, _ = self._optimize_model(
             eps,
             max_final_iterations,
-            lr,
+            best_model["lr"],
             random_init=False,
             init_iter=best_model["iteration"],
             init_logml=best_logml,
+            decay_rate=decay_rate,
+            decay_every=150,
         )
         print(
             "\n\n*****************************************************************************"
@@ -253,20 +264,18 @@ class GaussianProcess:
         ## inverse of K directly. Especially since it can make use of the fact that K is symmetric
         ## and positive definite. See https://www.johndcook.com/blog/2010/01/19/dont-invert-that-matrix/
         else:
-            from scipy import linalg
-
             solved = linalg.solve(self._K, K_obs_targets, assume_a="pos").T
             posterior_K = K_targets - solved @ K_obs_targets
             posterior_mean = mu_targets + solved @ (self._y - self._mu)
         #########################################################################
         ################# GENERATE SAMPLES #######################################
-        # nugget = 1e-9 * np.eye(posterior_K.shape[0])
+        # nugget = 1e-8 * np.eye(posterior_K.shape[0])
         # f = posterior_mean + np.linalg.cholesky(
-        # posterior_K + nugget
+            # posterior_K + nugget
         # ) @ np.random.normal(size=(x_targets.shape[0], 1))
         # Or:
         f = np.random.multivariate_normal(
-            posterior_mean.flatten(), posterior_K, check_valid="ignore"
+        posterior_mean.flatten(), posterior_K, check_valid="ignore"
         )
         # Take the diagonal to obtain the variance
         return f, posterior_mean.flatten(), np.diag(posterior_K).flatten()

minigauss/priors/covariance.py
@@ -21,8 +21,9 @@ class CovariancePrior(Prior, metaclass=abc.ABCMeta):
     def __init__(
         self, bounded_params: Dict[str, Bound], set_params: Dict[str, Any] = {}
     ) -> None:
-        # Every covariance kernel should have a noise std parameter
-        bounded_params["sigma_n"] = Bound(1e-3, 1)
+        if "sigma_n" not in bounded_params:
+            # Every covariance kernel should have a noise std parameter
+            bounded_params["sigma_n"] = Bound(1e-3, 1)
         super().__init__(bounded_params, set_params)
 
     @abc.abstractmethod
@@ -32,7 +33,7 @@ class CovariancePrior(Prior, metaclass=abc.ABCMeta):
     def __call__(self, x: np.ndarray, y: np.ndarray, observations=False) -> np.ndarray:
         K = self._covariance_mat(x, y)
         if observations:
-            K += np.eye(x.shape[0]) * self.sigma_n**2
+            K += self.sigma_n**2 * np.eye(x.shape[0])
         return K
 
     def optimize_one_step(
@@ -43,6 +44,9 @@ class CovariancePrior(Prior, metaclass=abc.ABCMeta):
         lr: float,
         optimizer="gradient_ascent",
     ) -> None:
+        """
+        This was most useful: https://math.stackexchange.com/questions/1030534/gradients-of-marginal-likelihood-of-gaussian-process-with-squared-exponential-co
+        """
         assert "mu" in ctx, "mu not set in context!"
         # Only add noise to the diagonal because we assume the observations to have independent
         # additive gaussian noise. The noise is added to the variance of the random variables,
@@ -50,22 +54,27 @@ class CovariancePrior(Prior, metaclass=abc.ABCMeta):
         K = self(x, x) + self.sigma_n**2 * np.eye(x.shape[0])
         K_inv = np.linalg.inv(K)
         residuals = y - ctx["mu"]
-        self._compute_gradients(x, y)
+        # Compute the gradient of K wrt the parameters of the kernel. K is the cov. mat. of (x,x).
+        self._compute_gradients(x, x)
         if optimizer == "gradient_ascent":
             for param in self._params.keys():
                 dK_dparam = self._grads[param]
                 K_inv_dot_dK_dparam = K_inv @ dK_dparam
                 lml_pd = 0.5 * (
-                    np.trace(K_inv_dot_dK_dparam)
-                    + (residuals.T @ dK_dparam @ K_inv_dot_dK_dparam @ residuals).item()
+                    -np.trace(K_inv_dot_dK_dparam)
+                    + (residuals.T @ K_inv @ dK_dparam @ K_inv @ residuals).item()
                 )
                 # Regularize this hyperparam because gradient ascent will definitely cheat
-                if param == "sigma_n":
-                    lml_pd *= 1e-5
+                # TODO: Figure this out because it only happens in some cases! I think mini-batches
+                # would actually fix it; the issue mostly (only?) arises when fitting a process from
+                # multiple realizations.
+                # if param == "sigma_n":
+                    # lml_pd *= 1e-1
                 self._params[param] += lr * lml_pd
                 # To help the optimization process, since those values should always be
                 # non-negative
-                self._params[param] = abs(self._params[param])
+                # TODO: Reparameterize all with exp(theta)
+                self._params[param] = max(1e-3, self._params[param])
         else:
             # TODO: More methods! https://gregorygundersen.com/blog/2022/03/20/conjugate-gradient-descent/
             raise NotImplementedError(f"Optimizer {optimizer} is not implemented.")
@@ -74,7 +83,9 @@ class CovariancePrior(Prior, metaclass=abc.ABCMeta):
 class ExponentialKernel(CovariancePrior):
     def __init__(
         self,
-        sigma_y_bounds: Bound = Bound(1e-3, 10),
+        sigma_y_bounds: Bound = Bound(1e-3, 1),
+        l_bounds: Bound = Bound(1e-3, 1),
+        sigma_n_bounds: Bound = Bound(1e-3, 1),
         sigma_y: Optional[float] = None,
         l: Optional[float] = None,
         sigma_n: Optional[float] = None,
@@ -88,7 +99,8 @@ class ExponentialKernel(CovariancePrior):
         super().__init__(
             {
                 "sigma_y": sigma_y_bounds,
-                "l": Bound(1e-3, 1),
+                "l": l_bounds,
+                "sigma_n": sigma_n_bounds,
             },
             set_params,
         )
@@ -100,23 +112,24 @@ class ExponentialKernel(CovariancePrior):
         denominator = 2 * (self.l + self._nugget_const) ** 2
         for i in range(K.shape[0]):
             K[i, :] = sigma_y_sqr * np.exp(
-                -np.sum((x[i, :] - y) ** 2, axis=1) / denominator
+                -np.sum((x[i] - y) ** 2, axis=1) / denominator
             )
         return K
 
     def _compute_gradients(self, x: np.ndarray, y: np.ndarray) -> None:
+        # TODO: Cache K's computation for x,y in the forward pass.
         def dCov_dSigmaY(x, y):
             K = np.zeros((x.shape[0], y.shape[0]))
             sigmay_2 = 2 * self.sigma_y
             denominator = 2 * (self.l + self._nugget_const) ** 2
             for i in range(K.shape[0]):
                 K[i, :] = sigmay_2 * np.exp(
-                    -np.sum((x[i, :] - y) ** 2, axis=1) / denominator
+                    -np.sum((x[i] - y) ** 2, axis=1) / denominator
                 )
             return K
 
         def dCov_dSigmaN(x, y):
-            return self.sigma_n * 2 * np.eye(x.shape[0])
+            return (2 * self.sigma_n) * np.eye(x.shape[0])
 
         def dCov_dSigmaL(x, y):
             K = np.zeros((x.shape[0], y.shape[0]))
@@ -125,14 +138,17 @@ class ExponentialKernel(CovariancePrior):
             for i in range(K.shape[0]):
                 residuals_sqr = np.sum((x[i, :] - y) ** 2, axis=1)
                 K[i, :] = (
-                    sigma_y_sqr * np.exp(-residuals_sqr / denominator) * residuals_sqr
-                ) / (self.l + self._nugget_const) ** 3
+                    sigma_y_sqr
+                    * np.exp(-residuals_sqr / denominator)
+                    * residuals_sqr
+                    / ((self.l + self._nugget_const) ** 3)
+                )
             return K
 
         self._grads = {
-            "sigma_y": dCov_dSigmaY(x, x),
-            "sigma_n": dCov_dSigmaN(x, x),
-            "l": dCov_dSigmaL(x, x),
+            "sigma_y": dCov_dSigmaY(x, y),
+            "sigma_n": dCov_dSigmaN(x, y),
+            "l": dCov_dSigmaL(x, y),
         }
 
 
@@ -195,7 +211,7 @@ class PeriodicKernel(CovariancePrior):
             return K
 
         self._grads = {
-            "sigma_y": dCov_dSigmaY(x, x),
-            "sigma_n": dCov_dSigmaN(x, x),
-            "l": dCov_dSigmaL(x, x),
+            "sigma_y": dCov_dSigmaY(x, y),
+            "sigma_n": dCov_dSigmaN(x, y),
+            "l": dCov_dSigmaL(x, y),
         }